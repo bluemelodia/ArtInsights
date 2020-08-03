47dd33378e9a2705cd7bd833ba03cabdb525d85b
blue <mlh2197@columbia.edu>
2020-08-02 20:04:33 -0400

    fix: restyle signup page to be more mobile-friendly.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -7,21 +8,42 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  /* 
-   * Creating these controls in this class gives us immediate access to 
-   * listen for, update, and validate the state of the form input. 
-   */
-  emailControl = new FormControl('');
-  passwordControl = new FormControl('');
+  loginForm: FormGroup; 
 
   public newUser = true;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private utils: UtilsService) { }
 
   ngOnInit() {
+    /* 
+    * The FormGroup instance provides its model value as an object reduced from the 
+    * values of each control in the group. Form group instances have the same properties
+    * (ex. value, untouched) and methods as a form control instance.
+    * 
+    * It tracks the status/changes for each control - so if one control changes, the parent
+    * control also emits a new status/value change. The model for the group is maintained
+    * from its members.  
+    * 
+    * Creating these FormControl controls in this class gives us immediate access to 
+    * listen for, update, and validate the state of the form input. 
+    */
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  /* Returns the form controls of the login form. */
+  get loginFormControl() {
+    console.log(this.loginForm.controls);
+    return this.loginForm.controls;
   }
 
   setLoginMode(isNewUser: boolean) {
     this.newUser = isNewUser;
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 }
