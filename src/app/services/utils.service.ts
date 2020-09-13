6f7aead38678e098c3c7f6895ab8c0c7c11b535c
blue <mlh2197@columbia.edu>
2020-09-12 22:07:27 -0400

    feat: use successive link removal ops.

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,29 @@ export class UtilsService {
   }
 
   public stripLinks(caption: string): string {
-    let captionFragments = caption.split("<a href=");
+    let captionWithoutHrefs = this.stripLinksUsingSplitter(caption, this.stripHrefLinks);
+    console.log("First split: ", captionWithoutHrefs);
+    let captionWithoutLinks = this.stripLinksUsingSplitter(captionWithoutHrefs, this.stripExternalLinks);
+    console.log("Second split: ", captionWithoutLinks);
+
+    return captionWithoutLinks;
+  }
+
+  private stripHrefLinks(caption: string) {
+    return caption.split("<a href=");
+  }
+
+  private stripExternalLinks(caption: string) {
+    return caption.split("<a class=\"external\" href=");
+  }
+
+  private stripLinksUsingSplitter(caption: string, splitter: Function) {
+    let captionFragments = splitter(caption);
     let newCaptionFragments: string[] = [];
-    captionFragments.forEach((str) => {
+    captionFragments.forEach((str: string) => {
       const slicedStr = str.slice(str.indexOf("</a>")+4);
       newCaptionFragments.push(slicedStr);
     });
-    console.log("NEW CAP FRAGMENTS: ", newCaptionFragments);
     return newCaptionFragments.join('');
   }
-
-
 }
