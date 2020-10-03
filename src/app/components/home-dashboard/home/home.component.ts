b1ceaf19945e13162b63029c7dbe1abd85f8a6e3
blue <mlh2197@columbia.edu>
2020-10-03 18:39:07 -0400

    feat: add loader logic to home.

--- src/app/components/home-dashboard/home/home.component.ts
+++ src/app/components/home-dashboard/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+import { LoadingService } from '../../../services/loading.service';
+import { Subscription, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  private routeObserver: Subscription;
 
-  constructor() { }
+  constructor(
+    private loading: LoadingService,
+    private router: Router
+  ) {
+    this.routeObserver = this.router.events
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(event => {
+        if (event instanceof NavigationEnd) {     
+          if (event.url === "/home") {
+            this.loading.hideLoader();
+          } 
+        }
+      });
+  }
 
   ngOnInit() {
   }
